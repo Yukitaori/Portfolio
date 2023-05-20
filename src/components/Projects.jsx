f7157b86d9bf942f2fadbe65d932feb4f6a32a1b
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-20 12:04:00 +0200

    changement d'icones + agrandissements d'images de projets

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Projects.module.css'
-import githubIcon from "../assets/icons/github_nr_64px.png"
+import githubIcon from "../assets/icons/github.png"
 import htmlicon from "../assets/icons/html-5.png"
 import cssicon from "../assets/icons/css-3.png"
 import jsicon from "../assets/icons/js.png"
@@ -21,24 +21,26 @@ function Projects() {
             <h3>Apr 2023 - May 2023</h3>
           </div>
           <div className={styles.projectStacks}>
-            <img className={styles.iconImg} src={htmlicon} alt="HTML icon" />
-            <img className={styles.iconImg} src={cssicon} alt="CSS icon" />
-            <img className={styles.iconImg} src={jsicon} alt="JavaScript icon" />
-            <img className={styles.iconImg} src={reacticon} alt="React icon" />
+            <img className={styles.stackImg} src={htmlicon} alt="HTML icon" />
+            <img className={styles.stackImg} src={cssicon} alt="CSS icon" />
+            <img className={styles.stackImg} src={jsicon} alt="JavaScript icon" />
+            <img className={styles.stackImg} src={reacticon} alt="React icon" />
           </div>
         </div>
-        <div className={styles.projectText}>
-          <p>Our second group projects is a video game that takes place in the Genshin Impact universe.</p>
-          <p>We had two constraints for this project : working with React and working with APIs.</p>
-          <p>We were three persons on this project and we worked for three weeks, building 3 mini-games using different algorithms. That was a lot of fun.</p>
+        <div className={styles.projectBody}>
+          <div className={styles.projectText}>
+            <p>Our second group projects is a video game that takes place in the Genshin Impact universe.</p>
+            <p>We had two constraints for this project : working with React and working with APIs.</p>
+            <p>We were three persons on this project and we worked for three weeks, building 3 mini-games using different algorithms. That was a lot of fun.</p>
           </div>
-        <img src={pickinimpact} alt="Pickin Impact Screenshot" />
+          <img className={styles.projectPic} onClick={(e) => e.target.className === styles.projectPic ? e.target.className = styles.projectPicLarge : e.target.className = styles.projectPic} src={pickinimpact} alt="Pickin Impact Screenshot" />
+        </div>
         <div className={styles.projectsLinks}>
           <a className={styles.projectLink} href="">
             <button className={styles.seeMore} type='button'>See more</button>
           </a>
-          <a className={styles.githubLink} href="">
-            <img src={githubIcon} alt="Github Icon" />
+          <a className={styles.githubLink} href="https://github.com/WildCodeSchool/2023-02-JS-FT-Lyon-P2-TradinImpact">
+            <img className={styles.iconImg} src={githubIcon} alt="Github Icon" />
           </a>
         </div>
       </div>
@@ -50,23 +52,25 @@ function Projects() {
             <h3>Mar 2023 - Apr 2023</h3>
           </div>
           <div className={styles.projectStacks}>
-            <img className={styles.iconImg} src={htmlicon} alt="HTML icon" />
-            <img className={styles.iconImg} src={cssicon} alt="CSS icon" />
-            <img className={styles.iconImg} src={jsicon} alt="JavaScript icon" />
+            <img className={styles.stackImg} src={htmlicon} alt="HTML icon" />
+            <img className={styles.stackImg} src={cssicon} alt="CSS icon" />
+            <img className={styles.stackImg} src={jsicon} alt="JavaScript icon" />
           </div>
         </div>
-        <div className={styles.projectText}>
-          <p>My first group project during the formation.</p>
-          <p>Four of us worked on this project for two weeks, that takes the form of a portfolio presenting the persons from the class, with a filters system.</p>
-          <p>Versioning with Git and Github were a bit of an intellectual challenge at first, but we survived it.</p>
+        <div className={styles.projectBody}>
+          <div className={styles.projectText}>
+            <p>My first group project during the formation.</p>
+            <p>Four of us worked on this project for two weeks, that takes the form of a portfolio presenting the persons from the class, with a filters system.</p>
+            <p>Versioning with Git and Github were a bit of an intellectual challenge at first, but we survived it.</p>
           </div>
-        <img src={wildkedin} alt="Wildked'in Screenshot" />
+          <img className={styles.projectPic} onClick={(e) => e.target.className === styles.projectPic ? e.target.className = styles.projectPicLarge : e.target.className = styles.projectPic} src={wildkedin} alt="Wildked'in Screenshot"  />
+        </div>
         <div className={styles.projectsLinks}>
-          <a className={styles.projectLink} href="">
+          <a className={styles.projectLink} href="https://wildcodeschool.github.io/2023-02-JS-FT-Lyon-P1-WeWildin/">
             <button className={styles.seeMore} type='button'>See more</button>
           </a>
-          <a className={styles.githubLink} href="">
-            <img src={githubIcon} alt="Github Icon" />
+          <a className={styles.githubLink} href="https://github.com/WildCodeSchool/2023-02-JS-FT-Lyon-P1-WeWildin">
+            <img className={styles.iconImg} src={githubIcon} alt="Github Icon" />
           </a>
         </div>
       </div>
@@ -78,23 +82,25 @@ function Projects() {
             <h3>Jan 2023 - Apr 2023</h3>
           </div>
           <div className={styles.projectStacks}>
-            <img className={styles.iconImg} src={htmlicon} alt="HTML icon" />
-            <img className={styles.iconImg} src={cssicon} alt="CSS icon" />
-            <img className={styles.iconImg} src={jsicon} alt="JavaScript icon" />
+            <img className={styles.stackImg} src={htmlicon} alt="HTML icon" />
+            <img className={styles.stackImg} src={cssicon} alt="CSS icon" />
+            <img className={styles.stackImg} src={jsicon} alt="JavaScript icon" />
           </div>
         </div>
-        <div className={styles.projectText}>
-          <p>A board games classic.</p>
-          <p>I started this project (my very first personal one) before my formation to test JavaScript and DOM manipulation.</p>
-          <p>I had quite a hard time with the algorithm in charge of verifying the user's guess but I eventually made it.</p>
+        <div className={styles.projectBody}>
+          <div className={styles.projectText}>
+            <p>A board games classic.</p>
+            <p>I started this project (my very first personal one) before my formation to test JavaScript and DOM manipulation.</p>
+            <p>I had quite a hard time with the algorithm in charge of verifying the user's guess but I eventually made it.</p>
           </div>
-        <img src={mastermind} alt="Mastermind Screenshot" />
+          <img className={styles.projectPic} onClick={(e) => e.target.className === styles.projectPic ? e.target.className = styles.projectPicLarge : e.target.className = styles.projectPic} alt="Mastermind Screenshot" src={mastermind} />
+        </div>
         <div className={styles.projectsLinks}>
-          <a className={styles.projectLink} href="">
+          <a className={styles.projectLink} href="https://yukitaori.github.io/Mastermind/">
             <button className={styles.seeMore} type='button'>See more</button>
           </a>
-          <a className={styles.githubLink} href="">
-            <img src={githubIcon} alt="Github Icon" />
+          <a className={styles.githubLink} href="https://github.com/Yukitaori/Mastermind">
+            <img className={styles.iconImg} src={githubIcon} alt="Github Icon" />
           </a>
         </div>
       </div>
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from "./Social.module.css";
-import github from "../assets/icons/github_nr_64px.png";
-import linkedin from "../assets/icons/linkedin_nr_64px.png";
-import mail from "../assets/icons/mail_nr_64px.png";
+import github from "../assets/icons/github.png";
+import linkedin from "../assets/icons/linkedin.png";
+import mail from "../assets/icons/mail.png";
 
 
 function Social() {
@@ -10,9 +10,9 @@ function Social() {
     <div>
       <h1>Social</h1>
       <div className={styles.socialIcons}>
-        <img src={github} alt="github icon" />
-        <img src={linkedin} alt="linkedin icon" />
-        <img src={mail} alt="mail icon" />
+        <img className={styles.socialIcon} src={github} alt="github icon" />
+        <img className={styles.socialIcon} src={linkedin} alt="linkedin icon" />
+        <img className={styles.socialIcon} src={mail} alt="mail icon" />
       </div>
     </div>
   )
