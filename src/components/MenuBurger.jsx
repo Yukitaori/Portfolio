c09005b38a450b75458180c362ce28c397c3eb3c
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-20 20:23:37 +0200

    réglages CSS

--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./MenuBurger.module.css";
 
 export default function MenuBurger({showMenu, setShowMenu}) {
+  const [checkbox, setCheckBox] = useState(<input id={styles.toggle} type="checkbox"></input>)
   const handleClick = () => {
     showMenu ? setShowMenu(false) : setShowMenu(true);
   }
+  useEffect(() => {
+    showMenu ? setCheckBox (<input id={styles.toggle} type="checkbox" checked></input>) : setCheckBox(<input id={styles.toggle} type="checkbox"></input>)
+  }, [showMenu])
   return (
-      <button className={styles.menuBurger} type="button" id="menuBurger" onClick={handleClick}>
-        Menu Burger
-      </button>
+    <div className={styles.menuBurgerDiv}>
+      {checkbox}
+      <label for={styles.toggle} id="menuBurger" className={styles.menuBurger} onClick={handleClick}>
+        <div className={styles.topBun}></div>
+        <div className={styles.meat}></div>
+        <div className={styles.bottomBun}></div>
+      </label>
+    </div>
   )
 }
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -10,9 +10,15 @@ function Social() {
     <div>
       <h1>Social</h1>
       <div className={styles.socialIcons}>
-        <img className={styles.socialIcon} src={github} alt="github icon" />
-        <img className={styles.socialIcon} src={linkedin} alt="linkedin icon" />
-        <img className={styles.socialIcon} src={mail} alt="mail icon" />
+        <a href="https://github.com/Yukitaori">
+          <img className={styles.socialIcon} src={github} alt="github icon" />
+        </a>
+        <a href="https://linkedin.com/in/francoischavallard">
+          <img className={styles.socialIcon} src={linkedin} alt="linkedin icon" />
+        </a>
+        <a href="mailto:francois.chavallard@gmail.com">
+          <img className={styles.socialIcon} src={mail} alt="mail icon" />
+        </a>
       </div>
     </div>
   )
