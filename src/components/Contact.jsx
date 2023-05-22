be57b9a1347d817979914d9dae5b862b3d526c5d
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-22 14:17:13 +0200

    menu animé + ajustements CSS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
         <form action="">
           <input type="text" placeholder='Enter your name' value={name} onChange={(e) => setName(e.target.value)} />
           <input type="text" placeholder='Enter your mail' value={mail} onChange={(e) => setMail(e.target.value)} />
-          <input type="text" placeholder='Enter your message here' value={message} onChange={(e) => setMessage(e.target.value)} />
+          <textarea type="text" placeholder='Enter your message here' rows="10" cols="30" value={message} onChange={(e) => setMessage(e.target.value)} />
           <button type="submit" disabled={name === '' || mail === '' || message === ''} onClick={(e) => e.preventDefault()}> Submit</button>
         </form>
       </div>
