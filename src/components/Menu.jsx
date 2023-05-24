f73323d076926cb1f02f0882ba14ea36d977c248
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-24 17:28:15 +0200

    test CSS pour menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,17 +5,20 @@ function Menu({parts, setShowMenu}) {
 
   const mapMenu = (arr) => {
     return arr.map((element) => {
-      return <li key={element} className={styles.navLinks} onClick={() => setShowMenu(false)}>
-        <a href={`#${element.toLowerCase().replaceAll(" ", "").replaceAll("'", "")}`}>
-        {`${element}`}
-        </a>
+      return (
+        <li key={element} className={styles.navLinks} onClick={() => setShowMenu(false)}>
+          <a href={`#${element.toLowerCase().replaceAll(" ", "").replaceAll("'", "")}`}>
+          {`${element}`}
+          </a>
         </li>
+      )
     });
   }
+  
   return(
     <ul className={styles.menu}>
-     {mapMenu(parts)}
-   </ul>
+      {mapMenu(parts)}
+    </ul>
   )
 }
 
