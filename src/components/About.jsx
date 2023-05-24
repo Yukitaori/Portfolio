e784af7b58730d60c6b07a7c1b79d2742dfbe6eb
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-24 17:17:17 +0200

    rectif texte + test CSS pour réglage bug menu lorsque le projet est déployé

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ function About() {
         <div className={styles.aboutText}>
           <p>I'm a guy from Villeurbanne, France.</p>
           <p>I've worked for years in the field of audiovisual production and cultural events organization, and then for ten years in the field of social administration.</p>
-          <p>I've studied development languages from february to july 2023, and I'm today proud to be a junior developer with some projects to show.</p>
+          <p>I'm studying development languages since february 2023 to july 2023, and I'm today proud to be a junior developer with some projects to show.</p>
           <p>Hope you enjoy.</p>
         </div>
         <img src={photo} className={styles.profilePic} alt="portrait of François" />
