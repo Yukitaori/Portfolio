340ed088a92cefe29bcb184e5d80414a34e782d3
Francois Chavallard <francois.chavallard@gmail.com>
2023-05-24 21:06:59 +0200

    tests

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,7 @@ function Menu({parts, setShowMenu}) {
   const mapMenu = (arr) => {
     return arr.map((element) => {
       return (
-        <li key={element} className="navLinks" onClick={() => setShowMenu(false)}>
+        <li key={element} className={styles.navLinks} onClick={() => setShowMenu(false)}>
           <a href={`#${element.toLowerCase().replaceAll(" ", "").replaceAll("'", "")}`}>
           {`${element}`}
           </a>
@@ -16,7 +16,7 @@ function Menu({parts, setShowMenu}) {
   }
   
   return(
-    <ul className="menuBar">
+    <ul className={styles.menuBar}>
       {mapMenu(parts)}
     </ul>
   )
