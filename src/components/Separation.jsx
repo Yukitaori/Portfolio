9124999d4cf833d66c6193c2a55aa3dbe49e96ea
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-05 14:42:36 +0200

    mise en place de séparations animées + responsive mobile mode paysage

--- src/components/Separation.jsx
+++ src/components/Separation.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import triskele from "../assets/icons/triskele.png"
+import styles from "./Separation.module.css"
 function Separation() {
+  const separationLine = [];
+
+  for (let i= 1; i <= 10; i++) {
+      separationLine.push(<img key={i} src={triskele} className={styles.triskele}/>)
+  }
+
   return (
-    <div className='separation'></div>
+    <div className={styles.separationLine}>
+      {separationLine.map(logo => logo)}
+    </div>
   )
 }
 
